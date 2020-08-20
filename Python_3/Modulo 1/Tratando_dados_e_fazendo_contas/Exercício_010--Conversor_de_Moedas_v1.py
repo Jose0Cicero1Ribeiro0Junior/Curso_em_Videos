9706ad1c57971bc7a4404ef58e1632df047b5a10
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 12:55:13 -0300

    Update Exercício_010--Conversor_de_Moedas_v1.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_010--Conversor_de_Moedas_v1.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_010--Conversor_de_Moedas_v1.py
@@ -1,1 +1,4 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+dolar = real / 5.63
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
