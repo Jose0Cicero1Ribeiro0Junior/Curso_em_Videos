33630e3c450810d1a24b4eb59665f46f93081290
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-19 23:43:09 -0300

    Update Exercício_009--Tabuada_v0.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_009--Tabuada_v0.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_009--Tabuada_v0.py
@@ -1,1 +1,19 @@
 #Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+from time import sleep
+txt = 'TABUADA'
+print(txt.center(40, '='))
+while True:
+    numero = str(input('Digite um número para a tabuada: '))
+    if numero.isnumeric() == True:
+        numero = int(numero)
+        break
+    else:
+        print('Erro!!!')
+for x in range(0, 11):
+    print(f'{x:2} X {numero:2} = ', end='')
+    if x * numero % 1 == 0:
+        print(x*numero)
+    else:
+        print(f'{x*numero:.1f}')
+    sleep(0.51)
+    
