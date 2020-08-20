96608861eb038cc9c7de4596a119714089892189
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 13:28:12 -0300

    Update Exercício_013--Reajuste_Salarial_v0.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_013--Reajuste_Salarial_v0.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_013--Reajuste_Salarial_v0.py
@@ -1,1 +1,9 @@
 #Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+salario = 'Salário do Funcionario que tiveram 15% de aumento este mês'
+print(salario.center(30, '='))
+while True:
+    salario = str(input('Digite o valor do salário: R$'))
+    if salario.isnumeric() == True:
+        salario = int(salario)
+        break
+print('O valor do produto é de {:.2f} com desconto de 5% fica por {:.2f}'.format(salario, salario + (salario* 15/100)))
