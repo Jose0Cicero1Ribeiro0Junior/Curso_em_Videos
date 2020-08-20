16f9d0d68672a3d9ee41382ecd575bc51de823f6
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 13:24:17 -0300

    Update Exercício_012--Calculando_Desconto_v0.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_012--Calculando_Desconto_v0.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_012--Calculando_Desconto_v0.py
@@ -1,1 +1,9 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+d = 'Descontão de 5% para qualquer produto '
+print(d.center(30, '='))
+while True:
+    preco = str(input('Digite o calor do produto: R$'))
+    if preco.isnumeric() == True:
+        preco = int(preco)
+        break
+print('O valor do produto é de {:.2f} com desconto de 5% fica por {:.2f}'.format(preco, preco - (preco* 5/100)))
