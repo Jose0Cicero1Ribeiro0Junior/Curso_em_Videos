b285c9dfa435d5304d65dc05b3b9c876cd95d69e
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 13:34:03 -0300

    Update Exercício_013--Reajuste_Salarial_v1.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_013--Reajuste_Salarial_v1.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_013--Reajuste_Salarial_v1.py
@@ -1,1 +1,4 @@
 #Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+sário = float(input('Qual é o salário do funcionário? R$'))
+novo = sário + sário * 15 / 100
+print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(sário, novo))
