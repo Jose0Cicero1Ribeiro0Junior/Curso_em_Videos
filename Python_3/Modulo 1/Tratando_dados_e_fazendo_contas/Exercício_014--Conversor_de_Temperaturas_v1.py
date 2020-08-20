c2aca7edae7f9699431f5f89bfb0f4ec450d2e86
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 14:03:26 -0300

    Update Exercício_014--Conversor_de_Temperaturas_v1.py

---
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_014--Conversor_de_Temperaturas_v1.py
@@ -0,0 +1,4 @@
+#Escreva um program que converta uma temperatura digita em °C e converta para  °F
+c = float(input('Informe a temperatura em °C: '))
+f = 9* c /5 +32
+print('A temperatura de {}°C corresponde a {}°F'.format(c, f))
