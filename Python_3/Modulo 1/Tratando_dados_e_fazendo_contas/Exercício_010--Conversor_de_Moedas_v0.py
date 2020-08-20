cbcb2075228fbb1bf766754e7c97210c130d1e42
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 12:51:12 -0300

    Update Exercício_010--Conversor_de_Moedas_v0.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_010--Conversor_de_Moedas_v0.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_010--Conversor_de_Moedas_v0.py
@@ -1,1 +1,9 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar.
+n = ' Conversor de dolar em real '
+print(n.center(40, '='))
+while True:
+    n = str(input('Quanto você tem na carteira? '))
+    if n.isnumeric() == True:
+        n = int(n)
+        break
+print(f'Você tem ${n/5.63:.2f}')
