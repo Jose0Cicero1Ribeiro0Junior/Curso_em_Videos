1f391d61637ecf77322b4453f6496d2d5c850807
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-20 13:10:39 -0300

    Update Exercício_011--Pintando_Paredes_v0.py

--- Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_011--Pintando_Paredes_v0.py
+++ Python_3/Modulo 1/Tratando_dados_e_fazendo_contas/Exercício_011--Pintando_Paredes_v0.py
@@ -1,1 +1,13 @@
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a área e a quantidade de tinta necessária para pinta-lá, sabendo que cada litro de tinta pinta uma área de 2m².
+nom = 'Calculo de Pintura de parede'
+print(nom.center(40,'='))
+while True:
+    b = str(input('Qual é a largura da parde: '))
+    h = str(input('Qual é a altura da parede: '))
+    if b.isnumeric() == h.isnumeric() == True:
+        b = int(b)
+        h = int(h)
+        break
+A = b*h
+litro_tinta = A/2
+print(f'A área de sua parede é de {A}, você tera que gastar {litro_tinta}L de tinta para poder pintar a parede.')
