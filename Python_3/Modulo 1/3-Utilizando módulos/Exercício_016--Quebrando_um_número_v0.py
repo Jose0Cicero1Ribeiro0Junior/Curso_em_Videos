608a1e7993efb8444069cf2d8b7c523d8376ce07
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-21 12:40:22 -0300

    Update Exercício_016--Quebrando_um_número_v0.py

--- Python_3/Modulo 1/3-Utilizando módulos/Exercício_016--Quebrando_um_número_v0.py
+++ Python_3/Modulo 1/3-Utilizando módulos/Exercício_016--Quebrando_um_número_v0.py
@@ -1,1 +1,14 @@
 #Crie um programa que leia em número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+def is_number(num):
+    try:
+        float(num)
+        return True
+    except:
+        pass
+    return False
+while True:
+    n = str(input('Digite um número Real qualquer: '))
+    if is_number(n) == True:
+        n = float(n)
+        break
+print(f'O valor digitado foi {n} e sua porção inteira é {n:.0f}')
