894b1b6ae7a35a0ab69495f58f49129f97c3ab22
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-22 19:18:43 -0300

    Create Exercício_020–Sorteando_uma_ordem_na_lista_v1.py

---
+++ Python_3/Modulo 1/3-Utilizando módulos/Exercício_020–Sorteando_uma_ordem_na_lista_v1.py
@@ -0,0 +1,1 @@
+#Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
