3ac4101f3162c969b6f09fce542c11120677b179
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-23 17:01:26 -0300

    Update Exercício_020–Sorteando_uma_ordem_na_lista_v0.py

--- Python_3/Modulo 1/3-Utilizando módulos/Exercício_020–Sorteando_uma_ordem_na_lista_v0.py
+++ Python_3/Modulo 1/3-Utilizando módulos/Exercício_020–Sorteando_uma_ordem_na_lista_v0.py
@@ -1,1 +1,16 @@
 #Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+from random import shuffle
+numer = 0 
+lista = []
+while True:
+    n1 = str(input('Nome do {}° aluno: '.format(numer+1)))
+    numer+=1
+    
+    if n1 == 'sair':
+        break
+    else:
+        lista.insert(numer,n1)
+
+shuffle(lista)
+for c in range(0,len(lista)):
+    print(f'{c+1}° - {lista[c]}')
