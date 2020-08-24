a85256a8481a02f21c3c368c1977853c0ff844a9
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-24 10:57:06 -0300

    Update Exercício_21–Tocando_um_MP3._v0.py

--- Python_3/Modulo 1/3-Utilizando módulos/Exercício_21–Tocando_um_MP3._v0.py
+++ Python_3/Modulo 1/3-Utilizando módulos/Exercício_21–Tocando_um_MP3._v0.py
@@ -1,2 +1,7 @@
-#Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
-import pygame 
+#Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3
+import playsound
+tru_false = str(input('Quer reproduzir a musica? ')).title()
+if tru_false == 'Sim':
+    playsound.playsound('/home/joseciceroribeirojunior/Música/Ghost.mp3')
+else:
+    print('Fim')
