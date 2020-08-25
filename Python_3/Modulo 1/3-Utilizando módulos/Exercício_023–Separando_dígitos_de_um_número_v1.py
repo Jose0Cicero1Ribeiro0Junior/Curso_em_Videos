1d1bfa36b9852ceb1113b63eb0f4a4c7dbac5758
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-08-25 10:52:57 -0300

    create

--- Python_3/Modulo 1/3-Utilizando módulos/Exercício_023–Separando_dígitos_de_um_número_v1.py
+++ Python_3/Modulo 1/3-Utilizando módulos/Exercício_023–Separando_dígitos_de_um_número_v1.py
@@ -1,1 +1,9 @@
 #Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+n = str(input('Informe um número: '))
+print('Analisando o número {}'.format(n))
+n = int(n)
+u = n // 1 % 10
+d = n // 10 % 10
+c = n // 100 % 10
+m = n // 1000 % 10
+print('Unidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}'.format(u,d,c,m))
