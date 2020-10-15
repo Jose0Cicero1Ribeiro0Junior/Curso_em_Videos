b178019fc25b7c8912068dfbd96ff938394b713c
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-10-14 22:07:50 -0300

    Update Exercício_098_Função_de_Contador.py

--- Python_3/Modulo 3/4 - Funções em Python/Exercício_098_Função_de_Contador.py
+++ Python_3/Modulo 3/4 - Funções em Python/Exercício_098_Função_de_Contador.py
@@ -3,3 +3,38 @@
 #b) de 10 até 0, de 2 em 2
 #c) uma contagem personalizada
 
+from time import sleep
+
+def contador(i,f,p):
+    if p < 0: 
+        p*= -1
+    if p == 0:
+        p = 1
+    
+    print('-='*20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <=f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i 
+        while cont >= f:
+            print(f'{cont} ',end='',flush=True)
+            sleep(0.5)
+            cont -=p
+        print('FIM!')
+
+contador(1,10,1)
+contador(10,0,2)
+print('-='*20) 
+print('Agora é a sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('Fim:    '))
+pas = int(input('Passo:  '))
+contador(ini,fim,pas)
