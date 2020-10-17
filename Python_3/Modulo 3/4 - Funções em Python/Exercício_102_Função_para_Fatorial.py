93e0070cc8c53723a5ac3be8bf5d72ddcf431985
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-10-17 07:14:36 -0300

    Update Exercício_102_Função_para_Fatorial.py

--- Python_3/Modulo 3/4 - Funções em Python/Exercício_102_Função_para_Fatorial.py
+++ Python_3/Modulo 3/4 - Funções em Python/Exercício_102_Função_para_Fatorial.py
@@ -7,11 +7,17 @@ def fatorial(n, show=False):
     :param show: (opcional) Mostar ou não a conta.
     :return: O valor do Fatorial de um número n.
     """
-
+   
     f = 1
     for c in range(n,0,-1):
         if show:
             print(c, end='')
             if c > 1:
-                print(' x ', end=' ')
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f 
+
+print(fatorial(5, show=True))
 
