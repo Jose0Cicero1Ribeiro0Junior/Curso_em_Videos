7ac5f5ee27417cfa97f3de6dab9320f729c9d7b6
JOSE CICERO RIBEIRO JUNIOR <Jose0Cicero1Ribeiro0Junior@outlook.com>
2020-10-17 08:38:40 -0300

    create

--- Python_3/Modulo 3/4 - Funções em Python/Exercício_104_Validando_entrada_de_dados_em_Python.py
+++ Python_3/Modulo 3/4 - Funções em Python/Exercício_104_Validando_entrada_de_dados_em_Python.py
@@ -4,7 +4,7 @@ def leiaInt(msg):
     ok = False
     valor = 0
     while True:
-        n = str(input(msg)):
+        n = str(input(msg))
         if n.isnumeric():
             valor = int(n)
             ok = True
---
+++ Python_3/Modulo 3/4 - Funções em Python/Exercício_105_Analisando_e_gerando_Dicionários.py
@@ -0,0 +1,34 @@
+#Exercício Python 105: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+
+#– Quantidade de notas  
+#– A maior nota
+#– A menor nota
+#– A média da turma
+#– A situação (opcional)
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionário com vários informações sobre a situação da turma
+    """
+
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n)/len(n)
+
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+resp = notas(5.5, 2.5, 1.5, sit=True)
+print(resp)
